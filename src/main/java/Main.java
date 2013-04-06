b1776ac7f3ce867206006d1e018d2813dfae8933
gclover <clover.gch@gmail.com>
2013-04-07 00:46:08 +0800

    change java format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,29 +11,31 @@ import cascading.tap.local.FileTap;
 import cascading.tuple.Fields;
 
 
-public class Main {
+public class Main 
+	{
 		
-	public static void main(String[] argv) {
+		public static void main(String[] argv) 
+			{
 		
-		Tap empTap = new FileTap(new TextLine(new Fields("empid")), "data/employee.txt");
-		Tap salesTap = new FileTap(new TextLine(new Fields("custid")), "data/salesfact.txt");
+			Tap empTap = new FileTap(new TextLine(new Fields("empid")), "data/employee.txt");
+			Tap salesTap = new FileTap(new TextLine(new Fields("custid")), "data/salesfact.txt");
 
-		Tap resultsTap = new FileTap(new TextLine(new Fields("empid1", "custid1")), "output/results.txt");
+			Tap resultsTap = new FileTap(new TextLine(new Fields("empid1", "custid1")), "output/results.txt");
 
-		Pipe empPipe = new Pipe("emp");
-		Pipe salesPipe = new Pipe("sales");
+			Pipe empPipe = new Pipe("emp");
+			Pipe salesPipe = new Pipe("sales");
 
-		Pipe join = new CoGroup(empPipe, new Fields("empid"), salesPipe, new Fields("custid"));
+			Pipe join = new CoGroup(empPipe, new Fields("empid"), salesPipe, new Fields("custid"));
 
-		FlowDef flowDef = FlowDef.flowDef()
-			.setName("flow")
-			.addSource(empPipe, empTap)
-			.addSource(salesPipe, salesTap)
-			.addTailSink(join, resultsTap);
+			FlowDef flowDef = FlowDef.flowDef()
+				.setName("flow")
+				.addSource(empPipe, empTap)
+				.addSource(salesPipe, salesTap)
+				.addTailSink(join, resultsTap);
 
-		FlowConnector connector = new LocalFlowConnector();
-		Flow flow = connector.connect(flowDef);
-		flow.complete();
+			FlowConnector connector = new LocalFlowConnector();
+			Flow flow = connector.connect(flowDef);
+			flow.complete();
 
+			}
 	}
-}
